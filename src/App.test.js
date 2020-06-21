8466a7aa0ed58d1ea0c827905b7453393d7c9389
kavinsan <kavin3103@hotmail.com>
2020-06-21 16:22:32 -0400

    Integrating Test

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
+import { shallow } from 'enzyme';
+import store from "./redux/store";
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+it('renders without crashing', () => {
+  shallow(<App store={store}/>);
 });
